7f3906ca8e9296d704d8d1bc8d4d1b3ce06da1bf
IKACE <guyile1998@gmail.com>
2021-03-13 22:05:34 +0800

    Finish implementing reading layers

--- darknet.py
+++ darknet.py
@@ -23,11 +23,12 @@ def read_cfg(cfgPath):
             netBlock["type"] = line[1:-1]
         else:
             split_strs = line.split("=")
-            netBlock[split_strs[0]] = split_strs[1]
+            netBlock[split_strs[0].rstrip()] = split_strs[1].lstrip()
     netList.append(netBlock)
     return netList
 
 def generate_modules(netList):
+    """Generate nn.ModuleList from list obtained from cfg file"""
     in_channels = 3
     output_filters = []
     modules = nn.ModuleList()
@@ -53,10 +54,60 @@ def generate_modules(netList):
             if batch_normalize != -1:
                 bn = nn.BatchNorm2d(out_channels)
                 module.add_module("bn{0}".format(idx), bn)
+        
+        elif netBlock["type"] == "upsample":
+            stride = netBlock["stride"]
+            upsample = nn.Upsample(scale_factor=stride, mode="bilinear")
+            module.add_module("upsample{0}".format(idx), upsample)
+        
+        elif netBlock["type"] == "shortcut":
+            shortcut = nn.Identity()
+            module.add_module("shortcut{0}".format(idx), shortcut)
+            skipFrom = int(netBlock["from"])
+            activation = netBlock["activation"]
+            relu = nn.LeakyReLU(0.01, True)
+            if activation == "leaky":
+                module.add_module("leaky{0}".format(idx), relu)
+            if output_filters[idx-1] != output_filters[idx+skipFrom]:
+                raise Exception("Dimensions do not match for shortcut layer at layer idx", idx)
+            out_channels = output_filters[idx-1]
+        
+        elif netBlock["type"] == "route":
+            layers = netBlock["layers"]
+            layers = layers.split(",")
+            route = nn.Identity()
+            module.add_module("route{0}".format(idx), route)
+            if len(layers) == 1:
+                routeFrom = int(layers[0])
+                if routeFrom < 0:
+                    out_channels = output_filters[idx+routeFrom]
+                else:
+                    out_channels = output_filters[routeFrom]
+            # this part is not generalized well, I just follow the format in the cfg file
+            elif len(layers) == 2:
+                left = int(layers[0])
+                right = int(layers[1])
+                if left >= 0 or right < 0:
+                    raise Exception("Unrecognized format of route layer for layer idx", idx, "please contact developer for help")
+                if right-idx >= 0:
+                    out_channels = output_filters[idx+left]
+                else:
+                    out_channels = output_filters[idx+left] + output_filters[right]
+        
+        elif netBlock["type"] == "yolo":
+            yolo = nn.Identity()
+            out_channels = output_filters[idx-1]
+            module.add_module("yolo{0}".format(idx), yolo)
+        in_channels = out_channels
+        output_filters.append(out_channels)
+        modules.append(module)
+    return modules
+                
 
 
 
 
 
 netList = read_cfg("cfg/yolov3.cfg")
-generate_modules(netList)
+modules = generate_modules(netList)
+print(modules)
